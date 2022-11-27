3773f9e1186c3040b489af30adc61f8886231c21
tanveerprottoy <tanveer.prottoy@gmail.com>
2022-11-27 17:46:23 +0600

    add semicolons

--- app.ts
+++ app.ts
@@ -7,17 +7,17 @@ const app = express();
 const port = 8080;
 
 // enabling cors for all requests by using cors middleware
-app.use(cors())
+app.use(cors());
 // parse requests of content-type: application/json
 // parses incoming requests with JSON payloads
-app.use(express.json())
+app.use(express.json());
 // parse requests of application/x-www-form-urlencoded
 app.use(express.urlencoded({
     extended: true
-}))
+}));
 
-app.use(Constants.API + Constants.VERSION_1 + '/users', userRouter)
+app.use(Constants.API + Constants.VERSION_1 + '/users', userRouter);
 
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
-})
+});
